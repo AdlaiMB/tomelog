533e7ed6ab4759b0d06f9ff78cd6e4ff9afa984f
Adlai M.B <adlaimb@gmail.com>
2026-01-08 13:28:06 -0800

    Move/Create contracts from searchImplementation to bookRepoInterface

--- searchImplementation.js
+++ searchImplementation.js
@@ -5,15 +5,11 @@ const assert = require("node:assert/strict");
 const bookRepoInterface = require("./bookRepoInterface.js");
 const bookRepoImplementation = require("./bookRepoImplementation.js");
 
-async function searchByTitle(title) {
-  const books = await bookRepoInterface.queryByTitle(
+function searchByTitle(title) {
+  return bookRepoInterface.queryByTitle(
     title,
     bookRepoImplementation.queryByTitle
   );
-
-  assert.strictEqual(books instanceof Array, true, "Books is not an array");
-
-  return books;
 }
 
 async function discoverySearch(search) {
