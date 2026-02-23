2ed750499221ceac9734e5857456dc2c33332613
Adlai M.B <adlaimb@gmail.com>
2026-02-22 22:00:17 -0800

    refactor the page content for the bookshelf route

--- src/components/bookshelf/App.jsx
+++ src/components/bookshelf/App.jsx
@@ -17,10 +17,65 @@ function App() {
 
   //   retrieveMyBooks();
   // }, []);
+  const [isMenuShowing, setIsMenuShowing] = useState(false);
 
   return (
     <>
       <NavBar page="bookshelf" />
+      <div className="page-content">
+        <div className="page-content-margin">
+          <div className="title-section">
+            <h1 className="title">bookshelf</h1>
+          </div>
+          <div className="tab-book-section">
+            <div className="tab-section">
+              <h2 className="tab-title">shelves</h2>
+              <div>
+                <div className="tabs-container">
+                  <div className="tabs">
+                    <button className="tab">reading</button>
+                    <button className="tab">read</button>
+                    <button className="tab">all books</button>
+                  </div>
+                </div>
+                <div className="drop-down-menu-section">
+                  <button
+                    onClick={() => setIsMenuShowing(!isMenuShowing)}
+                    className="tab-drop-down-menu-button"
+                  >
+                    dropdown
+                  </button>
+                  <div className="drop-down-menu-position-contianer">
+                    <div
+                      className={`drop-down-menu-container ${isMenuShowing ? "showing" : ""}`}
+                    >
+                      <div className="drop-down-menu-content">
+                        <ul className="drop-down-menu">
+                          <li>
+                            <button className="drop-down-menu-item-button">
+                              reading
+                            </button>
+                          </li>
+                          <li>
+                            <button className="drop-down-menu-item-button">
+                              read
+                            </button>
+                          </li>
+                          <li>
+                            <button className="drop-down-menu-item-button">
+                              all books
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
