776fec0ae0945409bee13f77bebd82a6a4ad7195
Adlai M.B <adlaimb@gmail.com>
2026-02-23 14:39:58 -0800

    refactor the navigation styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,18 @@
 import DropDownMenu from "./DropDownMenu";
+import "../styles/navigation/index.css";
 
 function NavBar() {
   const menuItems = [
-    <a href="/tomelog/search/" className="link-menu-item">
+    <a
+      href="/tomelog/search/"
+      className="link-menu-item white-text sen-regular background-light-brown-hover"
+    >
       search
     </a>,
-    <a href="/tomelog/bookshelf/" className="link-menu-item">
+    <a
+      href="/tomelog/bookshelf/"
+      className="link-menu-item white-text sen-regular background-light-brown-hover"
+    >
       bookshelf
     </a>,
   ];
@@ -14,18 +21,24 @@ function NavBar() {
     <header className="navigation-bar">
       <div className="navigation-bar-content">
         <a href="/" className="white-text">
-          <span className="logo">tomelog</span>
+          <span className="logo-text biorhyme-bold">tomelog</span>
         </a>
         <div>
           <nav className="navigation-nav">
             <ul className="navigation-nav-links">
               <li>
-                <a href="/tomelog/search/" className="navigation-nav-link">
+                <a
+                  href="/tomelog/search/"
+                  className="navigation-nav-link white-text sen-regular background-light-brown-hover"
+                >
                   search
                 </a>
               </li>
               <li>
-                <a href="/tomelog/bookshelf/" className="navigation-nav-link">
+                <a
+                  href="/tomelog/bookshelf/"
+                  className="navigation-nav-link white-text sen-regular background-light-brown-hover"
+                >
                   bookshelf
                 </a>
               </li>
